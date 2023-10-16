4d86678ccaced882be58c04cf8c0fe14348a2ae8
Suyono-Sukorame <suyonosukorame@ymail.com>
2023-10-16 21:39:32 +0800

    CRUD

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,25 +2,63 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
     private $taskList = [
-        'first' => 'sleep',
+        'first' => 'Sleep',
         'second' => 'Eat',
         'third' => 'Work',
     ];
 
-    public function index()
+    public function index(Request $request)
     {
-        if (request()->search) {
-            return $this->taskList[request()->search];
+        if ($request->search) {
+            $tasks = DB::table('tasks')
+                ->where('task', 'LIKE', "%$request->search%")
+                ->get();
+                return $tasks;
         }
-        return $taskList;
+        $tasks = Task::all();
+        return $tasks;        
+    }
+    
+    public function store(Request $request)
+    {
+        Task::create([
+            'task' => $request->task,
+            'user' => $request->user
+        ]);
+
+        return 'Success';
+    }
+
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return $task;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::find($id);
+        $task->update([
+            'task' => $request->task,
+            'user' => $request->user
+        ]);
+        return 'Success';
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        
+        $task->delete();
+
+        return 'Success';
     }
 }
-
-
-
-
--- routes/web.php
+++ routes/web.php
@@ -15,48 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
-
-
-
-// Route::get('about', function () {
-//     return view('about');
-// });
-
-// Route::get('/hello', function () {
-//     return response()->json([
-//         'message' => 'Hello, World'
-//     ]);
-// });
-
-// Route::get('/debug', function () {
-//     $dataArray = [
-//         'message' => 'Hello, World'
-//     ];
-//     ddd(request());
-// });
-
-
-
-Route::get('/task', [TaskController::class, 'index']);
-
-
-
-// Route::get('/task/{param}', function ($param) use ($taskList) {
-//     return $taskList[$param];
-// });
-
-// Route::post('/task', function () use ($taskList) {
-//     // return request()->all();
-//     $taskList[request()->label] = request()->task;
-//     return $taskList;
-// });
-
-// Route::patch('/task/{key}', function ($key) use ($taskList) {
-//     $taskList[$key] = request()->task;
-//     return $taskList;
-// });
-
-// Route::delete('/task/{key}', function ($key) use ($taskList) {
-//     unset($taskList[$key]);
-//     return $taskList;
-// });
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks/{id}', [TaskController::class, 'show']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::patch('/tasks/{id}', [TaskController::class, 'update']);
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
